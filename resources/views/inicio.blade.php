69004c8ccd21af36541af6d44ecb823ab723e109
Bryan Martinez <bmartinezg8@miumg.edu.gt>
2023-05-20 00:13:40 -0600

    Error 404 - excepciones

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -3,8 +3,33 @@
 @section('tituloPagina', 'Crud con laravel 8')
 
 @section('contenido')
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="">Transportes BryanM</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="jeje">INICIO</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">TRANSPORTES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">CAMIONES</a>
+                    </li>
+                </ul>
+                <span class="navbar-text">
+        0909-20-6903 PROGRAMACION III
+      </span>
+            </div>
+        </div>
+    </nav>
 
     <br><br>
+
 
     <div class="card">
         <h5 class="card-header text-center">Crud transportes</h5>
